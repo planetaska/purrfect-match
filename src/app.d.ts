832b2a009869617682f43de150df2c87a68e1341
rkappes <rkappes18@gmail.com>
2025-02-14 22:24:43 -0800

    files for supabase user authentication

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,19 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+import { SupabaseClient, Session } from '@supabase/supabase-js';
+
 declare global {
 	namespace App {
+		interface Locals {
+			supabase: SupabaseClient;
+			safeGetSession(): Promise<{ session: Session | null; user: User | null }>;
+		}
+		interface PageData {
+			session: Session | null;
+			user: User | null;
+		}
 		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
 		// interface Platform {}
 	}
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,35 @@
 <script lang="ts">
+	import '../styles.css';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	export let data;
+	// import { props } from '$props';
+	// const { data } = props;
+
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
+
 	let { children } = $props();
-	import "../app.css";
+	import '../app.css';
 </script>
+
+<svelte:head>
+	<title>User Management</title>
+</svelte:head>
+
+<div class="container" style="padding: 50px 0 100px 0">
+	<!-- {@render } -->
+</div>
 
 {@render children()}
