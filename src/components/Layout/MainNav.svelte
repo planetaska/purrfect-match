d3e5b40ba0052cbe5623a9621bc45e3d58723bed
planetaska <planetaska@gmail.com>
2025-02-18 15:03:28 -0800

    Add dark mode toggle

--- src/components/Layout/MainNav.svelte
+++ src/components/Layout/MainNav.svelte
@@ -3,8 +3,17 @@
 
 	let { supabase, user } = $props();
 	let showMobileMenu = $state(false)
+	let is_dark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
 
 	function toggleMobileMenu() { showMobileMenu = !showMobileMenu }
+	function toggleTheme() {
+		is_dark = !is_dark
+		if (is_dark) {
+			document.documentElement.classList.add('dark')
+		} else {
+			document.documentElement.classList.remove('dark')
+		}
+	}
 
 	// handles users sign out through Supabase
 	const signOut = async () => {
@@ -15,7 +24,7 @@
 	}
 </script>
 
-<header class="sticky inset-x-0 top-0 z-50 bg-white glass">
+<header class="sticky inset-x-0 top-0 z-50 bg-white dark:bg-gray-950 glass">
 	<nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
 		<div class="flex lg:flex-1">
 			<a href="/" class="-m-1.5 p-1.5 text-blue-300 flex items-center gap-x-2">
@@ -40,7 +49,31 @@
 			<a href="#" class="text-sm/6 font-semibold text-gray-900">My Favorites</a>
 			<a href="#" class="text-sm/6 font-semibold text-gray-900">About</a>
 		</div>
-		<div class="hidden lg:flex lg:flex-1 lg:justify-end lg:gap-x-8">
+		<div class="hidden lg:flex lg:flex-1 lg:justify-end lg:gap-x-8 lg:items-center">
+			<div>
+				<label class="swap swap-rotate">
+					<!-- this hidden checkbox controls the state -->
+					<input type="checkbox" class="theme-controller" value="dark" onchange={toggleTheme} />
+
+					<!-- sun icon -->
+					<svg
+						class="swap-off w-8 h-8 fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24">
+						<path
+							d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+					</svg>
+
+					<!-- moon icon -->
+					<svg
+						class="swap-on w-8 h-8 fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24">
+						<path
+							d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+					</svg>
+				</label>
+			</div>
 			{#if user}
 				<a href={`/users/${user.id}`} class="text-sm/6 font-semibold text-gray-900">My profile</a>
 				<button onclick={signOut} class="cursor-pointer text-sm/6 font-semibold text-gray-900">Sign out</button>
