1a433deb70dee81a0ac943d1aa2467544043b837
rkappes <rkappes18@gmail.com>
2025-02-19 20:15:24 -0800

    display pet type name and breed

--- src/routes/pets/[type]/+page.svelte
+++ src/routes/pets/[type]/+page.svelte
@@ -1,5 +1,17 @@
-<div>
-	This page should contain a list of pets with selected type and query parameters. e.g /pets/dogs?location_near=97211
-	<br>
+<script lang="ts">
+	// Just an example, suppose we have page data loaded from +page.server.ts:
+	//
+	import type { PageProps } from './$types';
+	let { data }: PageProps = $props();
+</script>
+
+<div class="text-base-900">
+	This page should contain a list of pets with selected type and query parameters. e.g
+	/pets/dogs?location_near=97211
+	<br />
 	Notice the dogs in the url is in plural form, this is a convention to represent a list of items.
 </div>
+
+{#each data.props.animal as pet}
+	<div>Pet type: {pet.type}, Name: {pet.name}, Breed: {pet.breeds['primary']}</div>
+{/each}
