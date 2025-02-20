67e6e40606031d45c04fa412b961b9fc8b088a8d
rkappes <rkappes18@gmail.com>
2025-02-19 20:16:21 -0800

    display pet name, breed, location (for now)

--- src/routes/pets/[type]/[id]/+page.svelte
+++ src/routes/pets/[type]/[id]/+page.svelte
@@ -1,5 +1,18 @@
+<script lang="ts">
+	// Just an example, suppose we have page data loaded from +page.server.ts:
+	//
+	import type { PageProps } from './$types';
+	let { data }: PageProps = $props();
+</script>
+
 <div>
 	This page should contain the info for a certain pet with supplied id. e.g /pets/dogs/123
-	<br>
+	<br />
 	123 is the id of the dog.
 </div>
+
+{#each data.props.animal as pet}
+	<div>
+		Pet type: {pet.type}, Name: {pet.name}, Breed: {pet.breeds['primary']}, Location: {pet.organization_id}
+	</div>
+{/each}
