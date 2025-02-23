dca69f902f86f1a82e75f2301b39232ae5d15a08
rkappes <rkappes18@gmail.com>
2025-02-22 23:59:09 -0800

    add action function to get data from form (for zip).

--- src/routes/pets/+page.server.ts
+++ src/routes/pets/+page.server.ts
@@ -1,21 +1,38 @@
 import type { PageServerLoad } from './$types'; //'@sveltejs/kit'; //
+import { redirect } from '@sveltejs/kit';
 
-export const load: PageServerLoad = async ({ fetch, params, parent }) => {
+export const actions = {
+	default: async ({ request }) => {
+		const data = await request.formData();
+		const zipcode = data.get('zipcode');
+
+		console.log(`Zip Code: ${zipcode}`);
+
+		if (zipcode) {
+			return redirect(303, `/pets?zipcode=${zipcode}`);
+		}
+	}
+};
+
+export const load: PageServerLoad = async ({ fetch, params, parent, url }) => {
 	try {
 		const { accessToken } = await parent();
+		const zip = url.searchParams.get('zipcode');
 
+		console.log(`In pets page.server load function, zip is ${zip}`);
 		const page = ''; //params.page;
-		let url = '';
-		if (accessToken) {
-			if (page) {
-				// url = `https://api.petfinder.com/v2/animals?type=Dog&type=Cat&page=${page}`;
-				url = `https://api.petfinder.com/v2/types?page=${page}`;
-			} else {
-				// url = `https://api.petfinder.com/v2/animals?type=Dog&type=Cat&page=1`;
-				url = `https://api.petfinder.com/v2/types?page=1`;
-			}
+		let path = '';
+		//const { page } = params.page;
+
+		if (page) {
+			// url = `https://api.petfinder.com/v2/animals?type=Dog&type=Cat&page=${page}`;
+			path = `https://api.petfinder.com/v2/types?page=${page}`;
+		} else {
+			// url = `https://api.petfinder.com/v2/animals?type=Dog&type=Cat&page=1`;
+			path = `https://api.petfinder.com/v2/types?page=1`;
 		}
-		const res = await fetch(url, {
+
+		const res = await fetch(path, {
 			method: 'GET',
 			headers: {
 				'Content-Type': 'application/json',
@@ -27,7 +44,7 @@ export const load: PageServerLoad = async ({ fetch, params, parent }) => {
 		// console.log(data);
 		return {
 			// props: { types: data.animals }
-			props: { types: data.types }
+			props: { types: data.types, zip }
 		};
 	} catch (error) {
 		console.error(`Error retreiving pets: ${error}`);
