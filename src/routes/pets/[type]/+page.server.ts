9d142787d2872bb0c57a9c6f935ae988a73e15c9
rkappes <rkappes18@gmail.com>
2025-02-23 00:00:33 -0800

    add actions to parse form data from /pets and redirect to pets/[type]

--- src/routes/pets/[type]/+page.server.ts
+++ src/routes/pets/[type]/+page.server.ts
@@ -1,19 +1,38 @@
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types'; //'@sveltejs/kit'; //
 
-export const load: PageServerLoad = async ({ fetch, params, parent }) => {
+export const actions = {
+	default: async ({ request }) => {
+		const data = await request.formData();
+		const type = data.get('type');
+		const zip = data.get('zipcode');
+
+		console.log(`type: ${type}`);
+		if (type) {
+			return redirect(303, `/pets/${type}?zipcode=${zip}`);
+		}
+	}
+};
+
+export const load: PageServerLoad = async ({ fetch, params, parent, url }) => {
 	try {
 		const { accessToken } = await parent();
+		const zip = url.searchParams.get('zipcode');
 		const { type } = params;
 
+		console.log(`zip is: ${zip}, type is ${type}`);
 		//Assume id for the location would be taken from user input
 		//https://api.petfinder.com/v2/animals?type=${type}&location=${id}
-		const res = await fetch(`https://api.petfinder.com/v2/animals?type=${type}`, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${accessToken}`
+		const res = await fetch(
+			`https://api.petfinder.com/v2/animals?type=${type}&location=${zip}&page=1`,
+			{
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${accessToken}`
+				}
 			}
-		});
+		);
 
 		const data = await res.json();
 		console.log(data);
