cae6ae1f1f1afbc97f65406131247109170d469b
planetaska <planetaska@gmail.com>
2025-02-28 13:25:39 -0800

    Add quiz ui

---
+++ src/components/Quiz/Quiz.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import QuizCard from '$components/Quiz/QuizCard.svelte';
+
+	const quizzes = [
+		{
+			question: 'What is the capital of France?',
+			answers: ['Paris', 'London', 'Berlin'],
+		},
+		{
+			question: 'What is 2 + 2?',
+			answers: ['3', '4', '5']
+		},
+		{
+			question: 'Which planet is known as the Red Planet?',
+			answers: ['Earth', 'Mars', 'Jupiter']
+		},
+		{
+			question: 'What is the largest mammal in the world?',
+			answers: ['Elephant', 'Blue Whale', 'Giraffe']
+		}
+	]
+
+</script>
+
+<div class="stack w-full max-w-lg">
+	{#each quizzes as quiz}
+		<QuizCard {quiz} />
+	{/each}
+</div>
--- src/routes/match/+page.svelte
+++ src/routes/match/+page.svelte
@@ -1,1 +1,31 @@
-This is the match page.
+<script lang="ts">
+	import Quiz from '$components/Quiz/Quiz.svelte';
+</script>
+
+<div class="relative overflow-hidden bg-base-100">
+	<!-- Decorative background image and gradient -->
+	<div aria-hidden="true" class="absolute inset-0">
+		<div class="absolute inset-0 mx-auto max-w-7xl overflow-hidden xl:px-8">
+			<img src="/images/layout/splash/purr-10.jpg" alt="" class="size-full object-cover">
+		</div>
+		<div class="absolute inset-0 bg-white/35"></div>
+		<div class="absolute inset-0 bg-linear-to-t from-base-100 via-base-100"></div>
+	</div>
+
+	<!-- Callout -->
+	<section aria-labelledby="callout-heading" class="relative mx-auto flex max-w-7xl flex-col items-center px-4 pt-32 text-center sm:px-6 lg:px-8">
+		<div class="mx-auto max-w-2xl lg:max-w-none">
+			<h2 id="sale-heading" class="text-4xl font-bold tracking-tight text-base-content sm:text-5xl lg:text-6xl">Let's find the purrfect match for you!</h2>
+			<p class="mx-auto mt-4 max-w-xl text-xl text-base-content">Tell us about yourself</p>
+		</div>
+	</section>
+
+	<!-- Questionnaires -->
+	<section aria-labelledby="questionnaires" class="relative mx-auto max-w-7xl px-4 py-24 sm:px-6 lg:px-8 lg:py-16">
+		<div class="mx-auto max-w-2xl lg:max-w-none">
+			<div class="mt-16 space-y-16 flex justify-center">
+				<Quiz />
+			</div>
+		</div>
+	</section>
+</div>
