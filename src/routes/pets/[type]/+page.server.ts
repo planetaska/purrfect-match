a87fda411be36450c7ae49022f22fa9b9317a656
rkappes <rkappes18@gmail.com>
2025-02-28 20:49:14 -0800

    parse data from filter form

--- src/routes/pets/[type]/+page.server.ts
+++ src/routes/pets/[type]/+page.server.ts
@@ -3,14 +3,35 @@ import type { PageServerLoad } from './$types'; //'@sveltejs/kit'; //
 
 export const actions = {
 	default: async ({ request }) => {
-		const data = await request.formData();
-		const type = data.get('type');
-		const zip = data.get('zipcode');
+		const url = new URL(request.url);
+		const formType = url.searchParams.get('form'); // Get the 'form' query parameter
 
-		console.log(`type: ${type}`);
-		if (type && zip) {
-			//Todo also check if zip exists
-			return redirect(303, `/pets/${type}?location=${zip}`);
+		const data = await request.formData();
+
+		if (formType === 'category') {
+			// const data = await request.formData();
+			const type = data.get('type');
+			const zip = data.get('zipcode');
+
+			console.log(`type: ${type}`);
+			if (type && zip) {
+				//Todo also check if zip exists
+				return redirect(303, `/pets/${type}?location=${zip}`);
+			}
+		} else if (formType == 'filter') {
+			// filter: async ({ request }) => {
+			// const data = await request.formData();
+			const type = data.get('type');
+			const zip = data.get('zipcode');
+			const size = data.getAll('size');
+			const gender = data.getAll('gender');
+			const age = data.getAll('age');
+			const children = data.get('children');
+			const dogs = data.get('dogs');
+			const cats = data.get('cats');
+
+			console.log(`type: ${type}, size: ${size.join(', ')}, gender: ${gender.join(', ')}
+				children: ${children}, dogs: ${dogs}, cats: ${cats}, age: ${age.join(', ')}`);
 		}
 	}
 };
