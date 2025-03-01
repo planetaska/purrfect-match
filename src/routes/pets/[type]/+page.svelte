45bfe1f35f91513176a37bfae01f75d3ef6cc4a7
rkappes <rkappes18@gmail.com>
2025-02-28 21:44:20 -0800

    only show message related to location if zip present

--- src/routes/pets/[type]/+page.svelte
+++ src/routes/pets/[type]/+page.svelte
@@ -5,14 +5,17 @@
 	let { data }: PageProps = $props();
 </script>
 
-<div class="text-base-900">
+<div>
 	This page should contain a list of pets with selected type and query parameters. e.g
 	/pets/dogs?location_near=97211
 	<br />
 	Notice the dogs in the url is in plural form, this is a convention to represent a list of items.
 </div>
 
-<p> Pets located near {data.props.zip}</p>
+{#if data.props.zip}
+	<p> Pets located near {data.props.zip}</p>
+{/if}
+
 {#each data.props.animal as pet}
 	<div>
 		Pet type: {pet.type}, Name: {pet.name}, Breed: {pet.breeds['primary']}, Location: {pet.organization_id}
