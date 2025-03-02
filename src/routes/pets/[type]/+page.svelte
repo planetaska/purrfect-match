397e2de9fc38ffb1ff446c8eb390b4718fed5fac
rkappes <rkappes18@gmail.com>
2025-03-01 17:42:43 -0800

    add check for data.props and remove unneeded <div>

--- src/routes/pets/[type]/+page.svelte
+++ src/routes/pets/[type]/+page.svelte
@@ -5,19 +5,14 @@
 	let { data }: PageProps = $props();
 </script>
 
-<div>
-	This page should contain a list of pets with selected type and query parameters. e.g
-	/pets/dogs?location_near=97211
-	<br />
-	Notice the dogs in the url is in plural form, this is a convention to represent a list of items.
-</div>
+{#if data.props}
+	{#if data.props.zip}
+		<p> Pets located near {data.props.zip}</p>
+	{/if}
 
-{#if data.props.zip}
-	<p> Pets located near {data.props.zip}</p>
+	{#each data.props.animal as pet}
+		<div>
+			Pet type: {pet.type}, Name: {pet.name}, Breed: {pet.breeds['primary']}, Location: {pet.organization_id}
+		</div>
+	{/each}
 {/if}
-
-{#each data.props.animal as pet}
-	<div>
-		Pet type: {pet.type}, Name: {pet.name}, Breed: {pet.breeds['primary']}, Location: {pet.organization_id}
-	</div>
-{/each}
