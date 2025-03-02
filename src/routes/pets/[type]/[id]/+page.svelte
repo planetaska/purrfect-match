ccbf8085a85e634620137b1ebe0228e90c869db3
rkappes <rkappes18@gmail.com>
2025-03-02 10:39:12 -0800

    display more pet info for individual pet

--- src/routes/pets/[type]/[id]/+page.svelte
+++ src/routes/pets/[type]/[id]/+page.svelte
@@ -5,15 +5,20 @@
 	let { data }: PageProps = $props();
 </script>
 
-<div>
-	This page should contain the info for a certain pet with supplied id. e.g /pets/dogs/123
-	<br />
-	123 is the id of the dog.
-</div>
-
-<!-- {#each data.props.animal as pet} -->
-<div>
-	Pet type: {data.props.animal.type}, ID: {data.props.animal.id}, Name: {data.props.animal.name},
-	Breed: {data.props.animal.breeds['primary']}
-</div>
-<!-- {/each} -->
+{#if data.props}
+	<div> 
+		<ul>
+			<li>Pet type: {data.props.animal.type} </li>
+			<li>ID: {data.props.animal.id} </li>
+			<li> Name: {data.props.animal.name}</li>
+			<li>Breed: {data.props.animal.breeds['primary']} </li>
+			<li>Age: {data.props.animal.age} </li>
+			<li>Gender: {data.props.animal.gender}</li>
+			<li>Size: {data.props.animal.size}</li>
+			<li>Description: {data.props.animal.description}</li>
+			<li>Good with children: {data.props.animal.environment['children']}</li>
+			<li>Good with dogs: {data.props.animal.environment['dogs']}</li>
+			<li>Good with cats: {data.props.animal.environment['cats']}</li>
+		</ul>
+	</div>
+{/if}
