4cfa805a3adc346c191e2e657ea5f499f216f833
rkappes <rkappes18@gmail.com>
2025-03-09 13:25:55 -0700

    initial display favorites

--- src/routes/users/my/favorites/+page.svelte
+++ src/routes/users/my/favorites/+page.svelte
@@ -1,1 +1,23 @@
-This is the user's favorites page.
+<script lang="ts">
+	import type { PageProps } from './$types';
+	let { data }: PageProps = $props()
+</script>
+
+<h1>Your Favorites</h1>
+
+{#if data && data.length > 0}
+  <ul>
+    {#each data.favorite as pet}
+      <li>
+        <div>
+          <img src={pet.petPhoto} alt={pet.petName} style="width: 100px; height: 100px;" />
+          <h2>{pet.petName}</h2>
+          <p>Type: {pet.petType}</p>
+          <a href={`/pets/type/${pet.petID}`} target="_blank">View Pet</a>
+        </div>
+      </li>
+    {/each}
+  </ul>
+{:else}
+  <p>You have no favorites yet.</p>
+{/if}
