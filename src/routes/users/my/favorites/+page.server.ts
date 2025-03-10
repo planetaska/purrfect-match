d38fef61d93439668cdf530e0f41259f04fcb727
rkappes <rkappes18@gmail.com>
2025-03-09 21:03:47 -0700

    query user's favorties from favorites table

--- src/routes/users/my/favorites/+page.server.ts
+++ src/routes/users/my/favorites/+page.server.ts
@@ -3,11 +3,20 @@ import type { PageServerLoad } from './$types';
 // Load function
 export const load: PageServerLoad = async ({ depends, locals: { supabase, user } }) => {
 	depends('supabase:db:favorites')
-	const { data: favorites } = await supabase.from('favorites')
-		.select('petType, petID, petName, petPhoto, petURL').eq('id', user?.id).then(response => {
+	const { data: favorites, error } = await supabase.from('favorites') //'petType, petID, petName, petPhoto, petURL'
+		.select('petType, petID, petName').eq('id', user?.id)
+		.then(response => {
+			console.log(response)
+			console.log(`User ID is ${user?.id}`)
 			return response
 		})
 
-	return { data: favorites}
+	if (error){
+		console.log(error)
+	}
+	else{
+		// console.log(favorites);
+		return { data: favorites }
+	}
 }
 
