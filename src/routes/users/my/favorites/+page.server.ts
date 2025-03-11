6a6848732fb4423d4664e91e6dbdf020746d8510
rkappes <rkappes18@gmail.com>
2025-03-11 13:25:29 -0700

    move favorites into /favorites route

---
+++ src/routes/users/my/favorites/+page.server.ts
@@ -0,0 +1,18 @@
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ depends, locals: { supabase, user } }) => {
+    depends('supabase:db:favorites')
+        const { data: favorites, error } = await supabase.from('favorites') //'petType, petID, petName, petPhoto, petURL'
+            .select('petType, petID, petName, petDescription, petURL').eq('id', user?.id)
+            .then(response => {
+                console.log(response)
+                console.log(`User ID is ${user?.id}`)
+                return response
+            })
+
+
+        if (error){
+            console.log(error)
+        }
+        return {favorites: favorites}
+}
