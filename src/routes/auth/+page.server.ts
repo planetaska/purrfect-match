3ac02d15cf59b5e50cb323f110279a93fbb958d4
rkappes <rkappes18@gmail.com>
2025-03-11 17:00:20 -0700

    add some console.log statments for debugging

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -70,14 +70,19 @@ export const actions: Actions = {
 		const newPassword = formData.get('new-password') as string
 		const confirmPassword = formData.get('confirm-password') as string
 
+		console.log(newPassword);
+
 		if (newPassword !== '' && confirmPassword === newPassword) {
 			const {error} = await supabase.auth.updateUser({ password: newPassword })
 			if (error) {
 				console.error(error)
 				return fail(500, { error})
 			} else {
+				console.log("succes!");
 				redirect(303, '/sign-in', { type: 'success', message: "Sign in" }, cookies)
 			}
+		} else{
+			console.log("passwords don't match!");
 		}
 	}
 }
