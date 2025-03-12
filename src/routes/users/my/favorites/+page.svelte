1519ac3ddb74794c7b32290450f875f5971292af
rkappes <rkappes18@gmail.com>
2025-03-11 18:48:00 -0700

    use Favorite component instead

--- src/routes/users/my/favorites/+page.svelte
+++ src/routes/users/my/favorites/+page.svelte
@@ -1,50 +1,19 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
+	import Favorites from '$components/Favorites.svelte';
+
 	let { data}: PageProps = $props()
-
-	async function deleteFav(id: number) {
-		const formData = new FormData();
-		formData.append('petId', id.toString());
-
-		const response = await fetch('/users/my/favorites', {
-			method: 'POST',
-			body: formData,
-		});
-
-		if (response.ok) {
-			// Successfully deleted pet, maybe navigate or refresh
-			alert('Pet deleted successfully!');
-		} else {
-			alert('Error deleting pet');
-		}
-  }
+	let { supabase } = $derived(data)
   
 </script>
 
 <div>
-    <!-- <input type="radio" name="user_tabs" class="tab" aria-label="Pet Favorites" /> -->
-		<!-- <div class="tab-content rounded-md overflow-hidden"> -->
-		
-			{#if data.favorites && data.favorites.length > 0}
-				{#each data.favorites as pet}
-				<div class="card card-side bg-base-100 shadow-sm">
-					<figure>
-					<img
-						src={pet.petPhoto} 
-						alt="pet" />
-					</figure>
-					<div class="card-body">
-					<h2 class="card-title">{pet.petName}</h2>
-					<p>{pet.petDescription}</p>
-					<div class="card-actions justify-end">
-						<button class="btn btn-active btn-secondary" onclick={()=>deleteFav(pet.petID)}>Delete</button>
-						<button class="btn btn-primary"> <a href="/pets/type/{pet.petID}"> View Details</a></button>
-					</div>
-					</div>
-				</div>
-				{/each}
-			{:else}
-			<p>You have no favorites yet.</p>
-			{/if}
-		<!-- </div> -->
+	{#if data.favorites && data.favorites.length > 0}
+	<h2 class="text-base/7 font-semibold text-base-content">Your Favorites</h2> 
+		{#each data.favorites as pet}
+		<Favorites {supabase} {pet} />
+		{/each}
+	{:else}
+	<h2 class="text-base/7 font-semibold text-base-content">You have no favorites yet</h2>
+	{/if}
 </div>
