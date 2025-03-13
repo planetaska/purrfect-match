0d363ae388198999c1d95e180ecf4ab99372e7b3
rkappes <rkappes18@gmail.com>
2025-03-13 14:29:58 -0700

    return to using a form to delete a pet record

--- src/routes/users/my/favorites/+page.server.ts
+++ src/routes/users/my/favorites/+page.server.ts
@@ -1,5 +1,23 @@
 import type { PageServerLoad } from './$types';
 
+export const actions = {
+    default: async ({ request, locals: { supabase} }) => {
+        const formData = await request.formData();
+        const petID = formData.get('petId') as string;
+
+        console.log(`PetID is ${petID}`);
+        // Delete the item from the Supabase table
+        const { data, error } = await supabase
+        .from('favorites') 
+        .delete()
+        .eq('petID', petID);
+
+        if (error) {
+            console.error('Error deleting item:', error);
+        } 
+    }
+}
+
 export const load: PageServerLoad = async ({ depends, locals: { supabase, user } }) => {
     depends('supabase:db:favorites')
         const { data: favorites, error } = await supabase.from('favorites') //'petType, petID, petName, petPhoto, petURL'
