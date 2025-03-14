a92e7be1c48766fd29ecf44e4f8f38d53d5f3296
rkappes <rkappes18@gmail.com>
2025-03-13 18:51:31 -0700

    add retreiving pet photo

--- src/routes/users/my/favorites/+page.server.ts
+++ src/routes/users/my/favorites/+page.server.ts
@@ -21,7 +21,7 @@ export const actions = {
 export const load: PageServerLoad = async ({ depends, locals: { supabase, user } }) => {
     depends('supabase:db:favorites')
         const { data: favorites, error } = await supabase.from('favorites') //'petType, petID, petName, petPhoto, petURL'
-            .select('petType, petID, petName, petDescription, petURL').eq('id', user?.id)
+            .select('petType, petID, petName, petDescription, petPhoto, petURL').eq('id', user?.id)
             .then(response => {
                 console.log(response)
                 console.log(`User ID is ${user?.id}`)
